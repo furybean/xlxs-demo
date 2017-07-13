3271269a7d2d0e752b610fad569bbc195c0f3623
Furybean <long.zhang@ele.me>
2017-07-13 18:41:02 +0800

    add xlsx

--- services/hello.js
+++ services/hello.js
@@ -3,6 +3,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 exports.handler = (event, ctx, callback) => {
-  console.log('invoke fc_helloworld!');
+  console.log('invoke helloworld!');
+
   callback(null, `Hello world! NODE_ENV: ${process.env.NODE_ENV}`);
 };
