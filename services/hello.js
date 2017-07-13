19a75cd807615c070d0293c87a7455c092e68549
Furybean <long.zhang@ele.me>
2017-07-13 19:00:51 +0800

    export buffer

--- services/hello.js
+++ services/hello.js
@@ -2,8 +2,12 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
+
 exports.handler = (event, ctx, callback) => {
   console.log('invoke helloworld!');
-
-  callback(null, `Hello world! NODE_ENV: ${process.env.NODE_ENV}`);
+// Or var xlsx = require('node-xlsx').default;
+  const xlsx = require('node-xlsx').default;
+  const data = [[1, 2, 3], [true, false, null, 'sheetjs'], ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
+  var buffer = xlsx.build([{name: "mySheetName", data: data}]); // Returns a buffer
+  callback(null, buffer);
 };
