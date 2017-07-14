175a44361556507ee96a608af4b036b46f9fbdb0
Furybean <long.zhang@ele.me>
2017-07-14 11:36:20 +0800

    add test body

--- services/hello.js
+++ services/hello.js
@@ -4,17 +4,9 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const B64 = require('b64');
 
 exports.handler = (event, ctx, callback) => {
-  console.log('invoke helloworld!');
-// Or var xlsx = require('node-xlsx').default;
+  console.log('invoke helloworld!', event);
   const xlsx = require('node-xlsx').default;
   const data = [[1, 2, 3], [true, false, null, 'sheetjs'], ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
-  var buffer = xlsx.build([{name: "mySheetName", data: data}]); // Returns a buffer
-  var a = {
-    headers: {
-      'Content-Type': 'application/octet-stream',
-      'Content-Disposition': 'filename="filename.xlsx"'
-    },
-    body: buffer
-  };
+  const buffer = xlsx.build([{ name: "Data", data: data }]);
   callback(null, { content: B64.encode(buffer).toString() });
 };
