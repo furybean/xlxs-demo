8bc192a2fb4a5fe4f9b58c7a7a13a4a020f84f80
Furybean <long.zhang@ele.me>
2017-07-14 11:47:19 +0800

    export excel by post data

--- services/hello.js
+++ services/hello.js
@@ -4,9 +4,17 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const B64 = require('b64');
 
 exports.handler = (event, ctx, callback) => {
-  console.log('invoke helloworld!', event);
+  console.log('invoke helloworld!');
   const xlsx = require('node-xlsx').default;
-  const data = [[1, 2, 3], [true, false, null, 'sheetjs'], ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
-  const buffer = xlsx.build([{ name: "Data", data: data }]);
+  let args;
+  try {
+    args = JSON.parse(event.toString());
+  } catch(e) {
+    args = {};
+  }
+  const data = args.data || [];
+  const header = args.header || [];
+  // const data = [[1, 2, 3], [true, false, null, 'sheetjs'], ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
+  const buffer = xlsx.build([{ name: "Data", data: header.concat(data) }]);
   callback(null, { content: B64.encode(buffer).toString() });
 };
